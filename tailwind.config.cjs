11f6fc7f935e8c77b41b7e23fe7d893443ad0977
Massimiliano Paolone <massimiliano.paolone95.kjosul@gmail.com>
2022-09-24 10:12:45 +0200

    First settings
    add TaiwindCSS, Astro and FontAwesome for the icons

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+      "./src/**/*.{astro,js,ts,jsx,tsx,vue}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          'test-blue': '#333377',
+        }
+      },
+    },
+    plugins: [],
+  }
