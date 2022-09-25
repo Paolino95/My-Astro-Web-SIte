4bafd8b2e4b15684e0868a23ad1662e88854b7b4
Massimiliano Paolone <massimiliano.paolone95.kjosul@gmail.com>
2022-09-25 15:22:18 +0200

    add Posts/What i Use pages and fix dropodown menù

--- src/components/vue/DropdownMenu.vue
+++ src/components/vue/DropdownMenu.vue
@@ -1,11 +1,14 @@
 <template>
-    <button class="block md:hidden" aria-label="menu">
+    <button class="block md:hidden" aria-label="menu" @click="toggleInput = !toggleInput">
         <i
             class="buttons-transition fa-solid fa-bars text-2xl hover:text-zinc-600 dark:hover:text-slate-400"
         ></i>
     </button>
-    <div class="block md:hidden absolute top-12 right-1 p-1 w-56 rounded-md">
-        <transition>
+    <transition>
+        <div
+            v-if="toggleInput === true"
+            class="block md:hidden absolute top-12 right-1 p-1 w-56 rounded-md"
+        >
             <div
                 class="flex flex-col overflow-auto text-sm z-10 bg-orange-50 border-[1px] border-zinc-900 dark:border-zinc-100 dark:bg-zinc-900 rounded-xl"
             >
@@ -18,13 +21,29 @@
                     >{{ item.name }}</a
                 >
             </div>
-        </transition>
-    </div>
+        </div>
+    </transition>
 </template>
 
 <script setup>
+    import { ref } from 'vue';
+
     const items = [
-        { name: 'Posts', href: './Posts' },
-        { name: `What I've use`, href: './WhatIUse' },
+        { name: 'Posts', href: './posts' },
+        { name: `What I've use`, href: './wiu' },
     ];
+
+    let toggleInput = ref(false);
 </script>
+
+<style scoped>
+    .v-enter-active,
+    .v-leave-active {
+        transition: opacity 0.5s ease;
+    }
+
+    .v-enter-from,
+    .v-leave-to {
+        opacity: 0;
+    }
+</style>
